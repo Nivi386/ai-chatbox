143cbc5ed688be498fc62a934be92cafb6530299
Nivi386 <nivetha382006@gmail.com>
2026-06-24 15:15:37 +0530

    ai chat working update

--- Backend/main.py
+++ Backend/main.py
@@ -2,9 +2,20 @@
 
 from fastapi import FastAPI
 from routes.chat import router as chat_router
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(chat_router)
+
 
 @app.get("/")
 def read_root():
